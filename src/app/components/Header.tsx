0ca6812e979347b4da24806a665e8020a7e9ae3d
renardbergson <renard.contato@gmail.com>
2025-08-28 12:21:30 -0300

    feat: inserting base to menu sheet

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,26 +3,84 @@
 import { Menubar, MenubarMenu } from "@/components/ui/menubar";
 import Image from "next/image";
 import { Menu } from "lucide-react";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+
+import { useState } from "react";
+import Link from "next/link";
 
 export const Header = () => {
+  const [sheetIsOpen, setSheetIsOpen] = useState(false);
+
   function handleMobileMenuClick() {
-    alert("Clicou no menu mobile");
+    setSheetIsOpen((prevState) => !prevState);
   }
 
   return (
-    <Menubar className="flex h-14 items-center bg-[#E5D4BC] p-3 text-[#562c17] shadow-md/25 shadow-black">
-      <MenubarMenu>
-        <div className="flex items-center">
-          <Image src={"/B.png"} alt="Teste" width={30} height={30} />
-          <h1 className="text-lg font-bold">Bonetto</h1>
-        </div>
+    <>
+      <Menubar className="flex h-16 items-center border-0 bg-[#E5D4BC] p-3 text-[#562c17] shadow-md/25 shadow-black">
+        <MenubarMenu>
+          <div className="flex items-center">
+            <Image src={"/B.png"} alt="Teste" width={30} height={30} />
+            <h1 className="ml-[2px] text-2xl font-bold">Bonetto</h1>
+          </div>
 
-        <div className="flex-1"></div>
+          <div className="flex-1"></div>
 
-        <button onClick={handleMobileMenuClick}>
-          <Menu />
-        </button>
-      </MenubarMenu>
-    </Menubar>
+          <button onClick={handleMobileMenuClick}>
+            <Menu />
+          </button>
+        </MenubarMenu>
+      </Menubar>
+
+      <Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
+        <SheetContent className="border-0 bg-[#e5d4bc] text-[#562c17]">
+          <SheetHeader className="flex flex-col">
+            <VisuallyHidden>
+              <SheetTitle>Menu Mobile</SheetTitle>
+            </VisuallyHidden>
+
+            <div className="mt-12 p-4">
+              <div>
+                <Link href={"#"} rel="Home Page">
+                  <h1 className="text-lg font-bold">Página Inicial</h1>
+                </Link>
+              </div>
+
+              <Separator className="!m-4 my-4 !ml-0 bg-[#d7c7b0]" />
+
+              <div>
+                <Link href={"#"} rel="Sobre Nós">
+                  <h1 className="text-lg font-bold">Monte seu design</h1>
+                </Link>
+              </div>
+
+              <Separator className="!m-4 my-4 !ml-0 bg-[#d7c7b0]" />
+
+              <div>
+                <Link href={"#"} rel="Sobre Nós">
+                  <h1 className="text-lg font-bold">Fale Conosco</h1>
+                </Link>
+              </div>
+
+              <Separator className="!m-4 my-4 !ml-0 bg-[#d7c7b0]" />
+
+              <div>
+                <Link href={"#"} rel="Sobre Nós">
+                  <h1 className="text-lg font-bold">Quem Somos</h1>
+                </Link>
+              </div>
+            </div>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </>
   );
 };
